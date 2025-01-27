def9f51fb2e721c3ca64ed1705d6d379dfecbe1d
Dias <dias.atudinov@gmail.com>
2025-01-27 12:38:39 +0500

    first commit

---
+++ Online HardR/Online HardR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Online HardR
+//
+//  Created by Dias Atudinov on 27.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Online HardR/Online HardR/Online_HardRApp.swift
@@ -0,0 +1,17 @@
+//
+//  Online_HardRApp.swift
+//  Online HardR
+//
+//  Created by Dias Atudinov on 27.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Online_HardRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
