c8513679a621d6542c3722a81af00141f8fcdb47
Dias <dias.atudinov@gmail.com>
2025-01-28 17:41:10 +0500

    Shop View

--- Online HardR/Online HardR/Modules/Shop/ShopView.swift
+++ Online HardR/Online HardR/Modules/Shop/ShopView.swift
@@ -8,11 +8,96 @@
 import SwiftUI
 
 struct ShopView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var shopVM: ShopViewModel
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                HStack {
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        ZStack {
+                            Image(.back)
+                                .resizable()
+                                .scaledToFit()
+                            
+                        }.frame(height: DeviceInfo.shared.deviceType == .pad ? 100:50)
+                        
+                    }
+                    Spacer()
+                }.padding([.leading, .top])
+                
+                HStack(spacing: 20) {
+                    ForEach(shopVM.shopItems, id: \.self) { item in
+                        Button {
+                            shopVM.currentItem = item
+                        } label: {
+                        ZStack {
+                            Image(.shopItemBg)
+                                .resizable()
+                                .scaledToFit()
+                            
+                            VStack {
+                                Text(item.name)
+                                    .font(.system(size: DeviceInfo.shared.deviceType == .pad ? 24:20, weight: .bold))
+                                    .foregroundStyle(.white)
+                                    .frame(width: 120)
+                                ZStack {
+                                    if item.name == "Creators of beauty" {
+                                        Image(.itemIcon2)
+                                            .resizable()
+                                            .scaledToFit()
+                                        
+                                        
+                                    } else if item.name == "Musical heaven" {
+                                        Image(.itemIcon1)
+                                            .resizable()
+                                            .scaledToFit()
+                                        
+                                    } else if item.name == "World classic" {
+                                        Image(.itemIcon3)
+                                            .resizable()
+                                            .scaledToFit()
+                                        
+                                    }
+                                    if let currentItem = shopVM.currentItem, currentItem.name == item.name {
+                                        VStack {
+                                            Spacer()
+                                            HStack {
+                                                Spacer()
+                                                Image(.tick)
+                                                    .resizable()
+                                                    .scaledToFit()
+                                                    .frame(height:DeviceInfo.shared.deviceType == .pad ? 100:50)
+                                            }.frame(width: DeviceInfo.shared.deviceType == .pad ? 300:150)
+                                        }
+                                    }
+                                }.frame(height: DeviceInfo.shared.deviceType == .pad ? 300:150)
+                                Spacer()
+                            }//.padding()
+                        }
+                            
+                            
+                        }.frame(height: DeviceInfo.shared.deviceType == .pad ? 550:290)
+                    }
+                }
+                
+                Spacer()
+            }
+        }.background(
+            ZStack {
+                Color.purpleBg.ignoresSafeArea()
+                Image(.settingsLines)
+                    .resizable()
+                    .ignoresSafeArea()
+                    .scaledToFill()
+            }
+        )
     }
 }
 
 #Preview {
-    ShopView()
+    ShopView(shopVM: ShopViewModel())
 }
--- Online HardR/Online HardR/Modules/Shop/ShopViewModel.swift
+++ Online HardR/Online HardR/Modules/Shop/ShopViewModel.swift
@@ -5,4 +5,50 @@
 //  Created by Dias Atudinov on 28.01.2025.
 //
 
-import Foundation
+import SwiftUI
+
+class ShopViewModel: ObservableObject {
+    @Published var shopItems: [Item] = [
+        Item(name: "Creators of beauty", images: ["inst1", "inst2", "inst3", "instEmpty", "inst4","inst5", "inst6", "inst7"]),
+        Item(name: "Musical heaven", images: ["music1", "music2", "music3", "musicEmpty", "music4","music5", "music6", "music7"]),
+        Item(name: "World classic", images: ["genre1", "genre2", "genre3", "genreEmpty", "genre4","genre5", "genre6", "genre7"]),
+
+    ]
+    
+    @Published var currentItem: Item? {
+        didSet {
+            saveTeam()
+        }
+    }
+    
+    init() {
+        loadTeam()
+    }
+    
+    private let userDefaultsTeamKey = "boughtItem"
+    
+    func saveTeam() {
+        if let currentItem = currentItem {
+            if let encodedData = try? JSONEncoder().encode(currentItem) {
+                UserDefaults.standard.set(encodedData, forKey: userDefaultsTeamKey)
+            }
+        }
+    }
+    
+    func loadTeam() {
+        if let savedData = UserDefaults.standard.data(forKey: userDefaultsTeamKey),
+           let loadedItem = try? JSONDecoder().decode(Item.self, from: savedData) {
+            currentItem = loadedItem
+        } else {
+            currentItem = shopItems[0]
+            print("No saved data found")
+        }
+    }
+    
+}
+
+struct Item: Codable, Hashable {
+    var id = UUID()
+    var name: String
+    var images: [String]
+}
